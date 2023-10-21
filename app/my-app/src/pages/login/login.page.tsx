4c5672efe577b283fba43930d19274076698d39e
Matthew Nguyen <mnguyen@777part.com>
2023-10-20 17:45:33 -0700

    updated login page with initial draft of page setup

--- app/my-app/src/pages/login/login.page.tsx
+++ app/my-app/src/pages/login/login.page.tsx
@@ -1,6 +1,22 @@
+import './index.css';
 
 export const LoginPage = () => {
   return (
-    <div>LOGIN PAGE</div>
+    <div className="login-page">
+      <div className="left-panel">
+        <div className="image-container">
+          <div className="image-box">
+          </div>
+        </div>
+      </div>
+      <div className="right-panel">
+        {/*
+          Components in this panel:
+            1. logo
+            2. signup-btn
+            3. login-form
+        */}
+      </div>
+    </div>
   )
 }
