de5e03d63eb01f4703a3f69f9116a5263beda492
Matthew Nguyen <mnguyen@777part.com>
2023-10-25 15:29:48 -0700

    updated user functions to utilize adding/authenticating users

--- internal/server/user.go
+++ internal/server/user.go
@@ -14,11 +14,14 @@ func signUp(ctx *gin.Context) {
 	// call Bind() to bind form data with User type
 	// if binding fails, set error code & message, and return from function
 	if err := ctx.Bind(user); err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := store.AddUser(user); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	// if no errors, set response code OK and return JWT for authentication
-	store.Users = append(store.Users, user)
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "Signed up successful.",
 		"jwt": "123456789",
@@ -28,17 +31,16 @@ func signUp(ctx *gin.Context) {
 func signIn(ctx *gin.Context) {
 	user := new(store.User)
 	if err := ctx.Bind(user); err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	for _, u := range store.Users {
-		if u.Username == user.Username && u.Password == user.Password {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "Signed in successful.",
-				"jwt": "1234567890",
-			})
-			return
-		}
+	user, err := store.Authenticate(user.Username, user.Password)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Sign in failed."})
+		return
 	}
-	ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"err": "Signed in failed."})
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "Signed in successful.",
+		"jwt": "1234567890",
+	})
 }
