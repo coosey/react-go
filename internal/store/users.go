f17da5fd8505e8b58ba034825ac81535542c5e11
Matthew Nguyen <mnguyen@777part.com>
2023-10-25 14:23:03 -0700

    added 2 functions to add and authenticate users

--- internal/store/users.go
+++ internal/store/users.go
@@ -1,8 +1,32 @@
 package store
 
+import "errors"
+
 type User struct {
+	ID       int
 	Username string `binding:"required,min=5,max=30"`
 	Password string `binding:"required,min=5,max=32"`
 }
 
 var Users []*User
+
+// nil is a predeclared identifier representing the zero value for a pointer, channel, func, interface, map, or slice type.
+func AddUser(user *User) error {
+	_, err := db.Model(user).Returning("*").Insert()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Authenticate(username, password string) (*User, error) {
+	user := new(User)
+	query := db.Model(user).Where("username = ?", username).Select()
+	if err := query; err != nil {
+		return nil, err
+	}
+	if password != user.Password {
+		return nil, errors.New("Invalid password.")
+	}
+	return user, nil
+}
